0895d255b3e5eae4cf9a4a815764167011a420c5
saikiran amballa <saikiran.amballa1037@gmail.com>
2017-05-29 16:42:06 +0530

    My Project Setup Completed Successfully

---
+++ onlinebackend/src/main/java/net/Saikiran1037/onlinebackend/App.java
@@ -0,0 +1,13 @@
+package net.Saikiran1037.onlinebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
